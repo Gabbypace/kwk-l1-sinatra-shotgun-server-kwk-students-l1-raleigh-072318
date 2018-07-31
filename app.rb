fdb14ca4b541380c0b2f1811487ecdb5d1bfbc48
Gabbypace <golfmarch03@gmail.com>
2018-07-31 14:29:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Look at me I'm a regular Danica Patrick!!!! "
+    "Look at me I'm a regular tiger woods!!! "
   end
 
 end
